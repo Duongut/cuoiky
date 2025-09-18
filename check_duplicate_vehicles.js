6a94cf7a128c212689ac1a72df2d41f7c086e33e
Châu Thái Dương <2251120075@ut.edu.vn>
2025-09-18 04:30:09 -0400

    Delete check duplicate

--- check_duplicate_vehicles.js
+++
@@ -1,8 +0,0 @@
-// Script to find duplicate vehicles with vehicleId M001
-db = db.getSiblingDB('SmartParkingDb');
-var duplicates = db.Vehicles.find({vehicleId: "M001"}).toArray();
-print("Found " + duplicates.length + " vehicles with vehicleId M001");
-duplicates.forEach(function(vehicle, index) {
-    print("Vehicle " + (index + 1) + ":");
-    printjson(vehicle);
-});
