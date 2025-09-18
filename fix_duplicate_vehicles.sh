618317631fa0057de1942c95ac77ebde82240b86
Châu Thái Dương <2251120075@ut.edu.vn>
2025-09-18 04:29:06 -0400

    Delete sh

--- fix_duplicate_vehicles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /home/user/ProjectITS/SmartParking.Core/SmartParking.Core
-
-# Compile and run the FixDuplicateVehiclesProgram
-dotnet run --project . --no-build FixDuplicateVehiclesProgram.cs
