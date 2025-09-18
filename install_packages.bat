033610ff94864104750bdfe7e4f5f84358f5d07f
Châu Thái Dương <2251120075@ut.edu.vn>
2025-09-18 04:42:15 -0400

    Delete install packages

--- install_packages.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Installing required NuGet packages...
-
-cd SmartParking.Core
-dotnet add SmartParking.Core package BCrypt.Net-Next
-dotnet add SmartParking.Core package Microsoft.AspNetCore.Authentication.JwtBearer
-
-echo Packages installed successfully.
-pause
